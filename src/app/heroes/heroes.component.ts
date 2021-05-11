480bf753c511fbfb2e96ec30df4aab1c26f7df32
marimorenova <39040673+marimorenova@users.noreply.github.com>
2021-05-11 22:20:07 +0200

    create messages component, create message service, inject it into hero service, send message from hero service, display message from hero service in message component, set message service as public to bind it, add additional messages to hero service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 
 @Component({
@@ -10,17 +12,20 @@ import { HeroService } from '../hero.service';
 })
 export class HeroesComponent implements OnInit {
   
-  heroes: Hero[] = [];
   selectedHero?: Hero;
 
-  constructor(private heroService: HeroService) { }
+  heroes: Hero[] = [];
+  
 
-  ngOnInit(): void {
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
+
+  ngOnInit(){
     this.getHeroes();
   }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}, name:${hero.name}`);
   }
 
   getHeroes(): void{
