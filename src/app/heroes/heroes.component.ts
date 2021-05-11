4cec73ed3883015ef280245e6104b615d4d15688
marimorenova <39040673+marimorenova@users.noreply.github.com>
2021-05-11 10:32:29 +0200

    create mock heroes, displaying heroes with *ngFor, styling the heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
+import { HEROES } from './../mock-heroes';
 import { Component, OnInit } from '@angular/core';
-import { Hero } from '../hero';
+
 
 @Component({
   selector: 'app-heroes',
@@ -7,10 +8,7 @@ import { Hero } from '../hero';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero : Hero = {
-    id:1,
-    name: 'Windstorm'
-  };
+  heroes = HEROES;
 
   constructor() { }
 
